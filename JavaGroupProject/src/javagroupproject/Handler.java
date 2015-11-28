7fabe854d7429f6bd829a89ab1c2b7db14c58f61
Lingyan Jiang <lynne326@hotmail.com>
2015-11-28 20:57:31 +1030

    weka

--- JavaGroupProject/src/javagroupproject/Handler.java
+++ JavaGroupProject/src/javagroupproject/Handler.java
@@ -9,12 +9,22 @@ import org.jfree.data.jdbc.JDBCCategoryDataset;
 import org.jfree.data.jdbc.JDBCPieDataset;
 import org.jfree.data.jdbc.JDBCXYDataset;
 import org.jfree.data.xy.XYDataset;
+
 /**
- * This class is to handle 
+ * This class is to handle JDBCDataset
  * @author lingyanjiang
  */
 public class Handler {
     
+    /**
+     * This method is to create chart with particular data type
+     * @param title chart title
+     * @param chartType 
+     * @param query sql query to extract data
+     * @return chartPanel
+     * @throws ClassNotFoundException
+     * @throws SQLException 
+     */
     public static ChartPanel getChart(String title,String chartType,String query) throws ClassNotFoundException, SQLException{
         //get dataset
         Dataset dataset = Dbconnection.connectDatabase(chartType, query);
@@ -26,14 +36,16 @@ public class Handler {
         ChartPanel p = null;
         
         if(dataset instanceof JDBCPieDataset){
+            //if pie set
             pieData = (PieDataset)dataset;
             p = JChart.generateChart(dataset, title, "pie");
         }
+        //if xy data set
         else if(dataset instanceof JDBCXYDataset){
             xyData = (XYDataset)dataset;
             p = JChart.generateChart(dataset, title, "histogram");
         }
-        
+        //if category
         else if(dataset instanceof JDBCCategoryDataset){
             categoryData = (CategoryDataset)dataset;
             if(chartType.equals("categoryx")){
@@ -53,6 +65,10 @@ public class Handler {
             
     }
     
+    /**
+     * This method is to get total records in database
+     * @return 
+     */
     public static int getTotalRecord() {
         return Dbconnection.connectDatabase(1);
     }
